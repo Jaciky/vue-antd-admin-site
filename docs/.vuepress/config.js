16f8cc38dc8d907b8e7c31d607647c858565c5e6
liubin_jaciky <liubin@qdsgvision.com>
2020-03-19 09:50:06 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: "vue-antd-admin",
+  description: "一个中后台单页项目",
+  base: "/blog/vue-antd-admin/",
+  head: [
+    [
+      "link",
+      {
+        rel: "icon",
+        href: "/favicon.ico"
+      }
+    ]
+  ]
+};
